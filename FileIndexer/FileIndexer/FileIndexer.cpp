fdf7d07ba640aa9b8f6e6288b4c6851c053b9f28
dsilva609 <dsilva609@gmail.com>
2015-01-30 22:10:33 -0500

    started thread logic

--- FileIndexer/FileIndexer/FileIndexer.cpp
+++ FileIndexer/FileIndexer/FileIndexer.cpp
@@ -1,20 +1,27 @@
 #include <map>
 #include <sstream>
+#include <thread>
 #include "FileParser.cpp"
 
 class FileIndexer
 {
 public:
-	void Execute(string filename)
+	void Execute(string filename, int numThreads)
 	{
-		//	this->_data = this->_parser.Read(filename);
-		this->IndexData();
+		this->_numThreads = numThreads;
+		//this->_data = this->_parser.Read(filename);
+		//this->DetermineIndices(numThreads);
+		//this->CreateThreads(numThreads);
+		//	this->IndexData();
 	}
 
 private:
-	vector<string> _data;
 	FileParser _parser;
+	vector<string> _data;
+	int _numThreads;
 	map<string, string> _dictionary;
+	vector<thread> _threads;
+	vector<int> _indices;
 
 	void IndexData()
 	{
@@ -26,7 +33,9 @@ private:
 			--detemine index locations for each word in string
 			--incement index per word not per line
 			-sort ascending when finished
-		*/
+
+			-need to handle rounding for division
+			*/
 
 		//test
 		string test = "It It is a truth universally acknowledged, that a single man in possession";
@@ -55,5 +64,35 @@ private:
 			currentIndex++;
 		}
 		//}
+	}
+
+	void DetermineIndices()
+	{
+		int offset, endIndex;
+
+		offset = this->_data.size() / this->_numThreads;
+		endIndex = 0;
+
+		for (int i = 0; i < this->_numThreads; i++)
+		{
+			endIndex += offset;
+			this->_indices.push_back(endIndex - offset);
+			this->_indices.push_back(endIndex);
+			endIndex++;
+		}
+	}
+
+	void CreateThreads()
+	{
+		for (int i = 0; i < this->_numThreads; i++)
+		{
+			this->_threads.push_back(thread(&FileIndexer::Created, this, i));
+			this->_threads.at(i).join();
+		}
+	}
+
+	void Created(int num)
+	{
+		cout << "created thread " << num << endl;
 	}
 };
